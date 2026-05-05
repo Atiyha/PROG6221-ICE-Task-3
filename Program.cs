ba453e6bb2fb689e0653c83ac81c2b7dadb1d4a8
ST10440871 <161321248+Atiyha@users.noreply.github.com>
2026-05-05 22:48:13 +0200

    push

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AlarmSystem
+{
+    // Task 1: Declare the delegate
+    delegate void AlarmHandler();
+
+    // Task 2: Create the Alarm class
+    class Alarm
+    {
+        // Declare the event using the delegate type
+        public event AlarmHandler OnAlarmTriggered;
+
+        // Method to trigger the alarm
+        public void TriggerAlarm()
+        {
+            Console.WriteLine("Alarm is triggered!");
+
+            // Call the event using Invoke()
+            // The null check ensures there are subscribers before invoking
+            OnAlarmTriggered?.Invoke();
+        }
+    }
+
+    class Program
+    {
+        // Task 3: Subscriber method
+        static void RespondToAlarm()
+        {
+            Console.WriteLine("Warning! Take action immediately!");
+        }
+
+        // Task 4: Main method
+        static void Main(string[] args)
+        {
+            // Create an object of Alarm
+            Alarm alarm = new Alarm();
+
+            // Subscribe RespondToAlarm to the event using +=
+            alarm.OnAlarmTriggered += RespondToAlarm;
+
+            // Trigger the alarm
+            alarm.TriggerAlarm();
+        }
+    }
+}
